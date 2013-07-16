46595c2e102a6740ccf9cb700c62962f99fc58c2
Erik Erwitt <eerwitt@gmail.com>
2013-07-16 13:37:04 -0700

    refactored code block to be an object to call

--- resources/public/javascript/tryclojure.js
+++ resources/public/javascript/tryclojure.js
@@ -1,52 +1,99 @@
-function eval_clojure(code, callback) {
-  $.ajax({
+// Pass in a JQuery HTML Element where the code editor should be, a callback for results and options
+function TryClojure(codeArea, resultCallback, params) {
+  params = params || {};
+  $.extend(true, params || {}, {
+    // Code block related
+    "autoFocus": true,
+    "autoMatchParens": true,
+    "autoCloseBrackets": true,
+    "lineNumbers": true,
+    "matchBrackets": true,
+    // Run related
+    "autoRun": false
+  });
+
+  // Private Variables
+  var _editorStatus = {
+    "dirty": true,
+    "autoRun": params.autoRun,
+    "lastCode": null,
+    "editor": null,
+    "timer": null
+  };
+
+  // Private Methods
+  function _init() {
+    _editorStatus.editor = _setupCodeBlock(codeArea, params, function() {
+      _editorStatus.dirty = true;
+    });
+
+    _runCode();
+  }
+
+  function _eval(code, callback) {
+    $.ajax({
       url: "eval.json",
       data: { expr : code },
       success: function(data) {
         callback(code, data);
       }
-  });
-}
-
-function evaluateCode(editorStatus, editor, callback) {
-  var currentCode = editor.getValue();
-  if(editorStatus.dirty && editorStatus.lastCode != currentCode) {
-    eval_clojure(currentCode, callback);
-
-    editorStatus.lastCode = currentCode;
-    editorStatus.dirty = false;
+    });
   }
-}
 
-function runCode(editorStatus, editor, callback) {
-  var codeEvaluator = function() {
-    evaluateCode(editorStatus, editor, callback);
+  function _setupCodeBlock(codeTextArea, params, onChangeCallback) {
+    var editor = CodeMirror.fromTextArea(codeTextArea, {
+      "autofocus": params.autoFocus,
+      "autoMatchParens": params.autoMatchParens,
+      "autoCloseBrackets": params.autoCloseBrackets,
+      "lineNumbers": params.lineNumbers,
+      "matchBrackets": params.matchBrackets});
+
+    editor.on('change', onChangeCallback);
+
+    return editor;
+  }
+
+  function _evaluateCode(editorStatus, currentCode, callback) {
+    if(editorStatus.dirty && editorStatus.lastCode != currentCode) {
+      callback();
+
+      editorStatus.lastCode = currentCode;
+      editorStatus.dirty = false;
+    }
+  }
+
+  function _runCode() {
+    var currentCode = _editorStatus.editor.getValue();
+    _evaluateCode(_editorStatus, currentCode, function() {
+      _eval(currentCode, function(code, data) {
+        resultCallback(code, data);
+      });
+    });
+  }
+
+  // Call Initialization Methods
+  _init();
+
+  // Public Methods
+  return {
+    "runButtonClick": function(evt) {
+        evt.preventDefault();
+        _runCode();
+    },
+    "autoRunClick": function(turnOn, turnOff) {
+      if(_editorStatus.autoRun) {
+        turnOff();
+        clearInterval(_editorStatus.timer);
+      } else {
+        turnOn();
+        _editorStatus.timer = setInterval(_runCode, 2000);
+        _runCode();
+      }
+      _editorStatus.autoRun = !_editorStatus.autoRun;
+    },
   };
-
-  codeEvaluator();
-
-  return codeEvaluator;
 }
 
-function setupCodeBlock(codeTextArea, callback) {
-  var editor = CodeMirror.fromTextArea(codeTextArea, {
-    "autofocus": true,
-    "autoMatchParens": true,
-    "autoCloseBrackets": true,
-    "lineNumbers": true,
-    "matchBrackets": true});
-
-  var editorStatus = {
-    "dirty": true,
-    "lastCode": null
-  };
-
-  editor.on('change', function() {
-    editorStatus.dirty = true;
-  });
-
-  return runCode(editorStatus, editor, callback);
-}
 
 $(document).ready(function() {
   $(".code").each(function(i, element) {
@@ -58,7 +105,7 @@ $(document).ready(function() {
     var runButton = $(codeBlockSelector);
     var autoRunButton = $(codeBlockSelector + "a[data-autorun='true']");
 
-    var responseCallback = function(originalCode, response) {
+    var resultCallback = function(originalCode, response) {
       if(!response.error && !expectedResult) {
         resultDiv.removeClass("text-error");
         resultDiv.addClass("text-success");
@@ -74,20 +121,20 @@ $(document).ready(function() {
       }
     };
 
-    var codeEvaluator = setupCodeBlock(element, responseCallback);
+    var tryClojure = new TryClojure(element, resultCallback);
 
     if(runButton) {
-      runButton.on('click', function(evt) {
-        evt.preventDefault();
-        codeEvaluator();
-      });
+      runButton.on('click', tryClojure.runButtonClick);
     }
 
     if(autoRunButton) {
       autoRunButton.on('click', function(evt) {
-        $(this).find("i").toggleClass("icon-spin").toggleClass("running");
-        setInterval(codeEvaluator, 2000);
-        codeEvaluator();
+        var icon = $(this).find("i");
+        tryClojure.autoRunClick(function() {
+          icon.toggleClass("icon-spin").toggleClass("running");
+        }, function() {
+          icon.toggleClass("icon-spin").toggleClass("running");
+        });
       });
     }
   });
